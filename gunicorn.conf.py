7e7749b97abaa954f2f9eff06d808eacd4420b0d
kylebd99 <kylebd99@gmail.com>
2025-08-15 22:40:34 +0000

    add multiple workers

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -6,12 +6,12 @@ def _get_env(key: str, default: str) -> str:
     return value if value not in (None, "") else default
 
 bind = _get_env("GUNICORN_BIND", "0.0.0.0:8080")
-workers = int(_get_env("GUNICORN_WORKERS", "1"))
-threads = int(_get_env("GUNICORN_THREADS", str(max(2, multiprocessing.cpu_count() * 2 + 1))))
+workers = int(_get_env("GUNICORN_WORKERS", "24"))
+threads = int(_get_env("GUNICORN_THREADS", "4"))
 timeout = int(_get_env("GUNICORN_TIMEOUT", "120"))
 graceful_timeout = int(_get_env("GUNICORN_GRACEFUL_TIMEOUT", "120"))
 keepalive = int(_get_env("GUNICORN_KEEPALIVE", "5"))
-worker_class = _get_env("GUNICORN_WORKER_CLASS", "sync")
+worker_class = _get_env("GUNICORN_WORKER_CLASS", "gthread")
 accesslog = _get_env("GUNICORN_ACCESSLOG", "-")
 errorlog = _get_env("GUNICORN_ERRORLOG", "-")
 loglevel = _get_env("GUNICORN_LOGLEVEL", "info")
@@ -21,4 +21,4 @@ max_requests = int(_get_env("GUNICORN_MAX_REQUESTS", "1000"))
 max_requests_jitter = int(_get_env("GUNICORN_MAX_REQUESTS_JITTER", "100"))
 
 # Preload app: False by default because models are heavy; set True if we want faster worker boot
-preload_app = _get_env("GUNICORN_PRELOAD_APP", "false").lower() in ("1", "true", "yes")
+preload_app = _get_env("GUNICORN_PRELOAD_APP", "true").lower() in ("1", "true", "yes")
