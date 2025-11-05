8d7e276e712e0a20ebfdc59cacb1742a679e4a4c
kylebd99 <kylebd99@gmail.com>
2025-11-05 21:41:26 +0000

    Update S3 prefix to prod-serving and change keyword index type to SQLite in server setup script

--- scripts/server_setup.sh
+++ scripts/server_setup.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Server setup
-s3_prefix="s3://bcgl-public-bucket/test-serving"
+s3_prefix="s3://bcgl-public-bucket/prod-serving"
 data_dir="data/prod" # 'dev-serving', 'test-serving', or 'prod-serving'
 #s5cmd sync $s3_prefix/embeddings/* data/prod/embeddings
 #s5cmd sync $s3_prefix/embeddings_img_pg/* data/prod/embeddings_img_pg
@@ -13,4 +13,4 @@ s5cmd sync $s3_prefix/index_keyword/* data/prod/index_keyword
 s5cmd sync $s3_prefix/index_metadata/* data/prod/index_metadata
 #s5cmd sync $s3_prefix/metadata/* data/prod/metadata
 
-poetry run python scripts/python_helpers/run_gunicorn.py --data-directory ./data/prod --text_model BGE --visual_model CLIP --keyword_index_type LanceDB --vector_index_type Memory 
+poetry run python scripts/python_helpers/run_gunicorn.py --data-directory ./data/prod --text_model BGE --visual_model CLIP --keyword_index_type SQLite --vector_index_type Memory 
