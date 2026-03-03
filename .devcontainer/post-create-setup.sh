6022dc282fc0082a723ee700a68b05b87592ef7f
Albert Du <52804499+albert-du@users.noreply.github.com>
2026-03-03 17:52:02 +0000

    improve devcontainer post create setup

--- .devcontainer/post-create-setup.sh
+++ .devcontainer/post-create-setup.sh
@@ -5,12 +5,17 @@ echo ""
 echo "____________________________________________________________________________________________"
 echo ""
 
+# Check for .containervenv and create it if it doesn't exist
+if [ ! -d ".containervenv" ]; then
+    echo "Creating .containervenv virtual environment..."
+    python -m venv .containervenv --system-site-packages
+else
+    echo ".containervenv virtual environment already exists. Skipping creation."
+fi
+
 # we need to activate the Poetry environment for the user to use it in the terminal
 # so edit the .bashrc file to activate the Poetry environment automatically when the terminal is opened
 echo "Activating Poetry environment..."
-
-test python -m venv .containervenv --system-site-packages
-
 echo "eval \$(poetry env activate)" >> ~/.bashrc
 # also for zsh users, we need to add it to the .zshrc file
 echo "eval \$(poetry env activate)" >> ~/.zshrc
@@ -19,16 +24,16 @@ echo ""
 echo "____________________________________________________________________________________________"
 echo ""
 
-echo "Installing pre-commit hooks..."
-poetry run pre-commit install
-
+echo "Installing Python dependencies with Poetry..."
+poetry install
 
 echo ""
 echo "____________________________________________________________________________________________"
 echo ""
 
-echo "Installing Python dependencies with Poetry..."
-poetry install
+# Must happen after poetry install, otherwise pre-commit won't be available
+echo "Installing pre-commit hooks..."
+poetry run pre-commit install
 
 echo ""
 echo "____________________________________________________________________________________________"
